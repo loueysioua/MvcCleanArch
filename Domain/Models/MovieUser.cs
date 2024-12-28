76941719155582c56cb7c6ebc95caefc2bfffc93
loueysioua <louey.sioua@insat.ucar.tn>
2024-12-28 20:34:51 +0100

    fix: MovieUser

--- Domain/Models/MovieUser.cs
+++ Domain/Models/MovieUser.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,6 +14,8 @@ namespace MvcCleanArch.Domain.Models
     public Guid MovieId { get; set; }
     [Key]
     public required string UserId { get; set; }
+
+    [DefaultValue(false)]
     public bool IsFavourite { get; set; }
     public virtual required Movie Movie { get; set; }
     public virtual required AppUser User { get; set; }
